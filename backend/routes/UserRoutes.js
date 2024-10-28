480c46c8530c6a26a8c68c3262ffb867ff6f69b9
mikek182 <marcosvinicius@geekwork.com.br>
2024-10-28 12:41:31 -0300

    Criação da roda de update

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -6,5 +6,6 @@ router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id', UserController.editUser)
 
 module.exports = router 
