dda0e2b96c87884d479477432ce1fb5985ceabaf
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-02 14:02:16 -0300

    Inserindo logo

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom';
+import logo from '../../assets/img/logo.png'
 
 function Navbar() {
     return (
        <nav>
+            <div>
+                <img src={logo} alt='Quero um pet' />
+                <h2>Quero um pet</h2>
+            </div>
             <ul>
                 <li><Link to="/">Adotar</Link></li>
                 <li><Link to="/login">Entrar</Link></li>
