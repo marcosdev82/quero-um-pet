f2cf31037835f9037ac01ce2653a333c0183ce2e
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-02 10:39:34 -0300

    Método update schedule

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -227,5 +227,52 @@ module.exports =  class PetController {
     return res.status(200).json({ message: 'Pet atualizado com sucesso!' });
   }
 
+  static async schedule(req, res) {
+
+    const id = req.params.id
+
+    // check if pet exists
+    const pet = await Pet.findOne({ _id: id });
+  
+    if (!pet) {
+      return res.status(404).json({ message: 'Pet não encontrado!' });
+    }
+
+    // check user registered the pet
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if (pet.user._id.equals(user._id.toString())) {
+      return res.status(422).json({
+          message: 'Você não pode agendar uma visita com seu próprio pet!'
+      });
+    }
+
+    // check if user has already scheduled a visit
+    if (pet.adopter) {
+      if(pet.adopter._id.equals(user._id)) {
+        return res.status(422).json({
+          message: 'Você já agendou uma visita para este pet!'
+        });
+      }
+    }
+
+    // add user to pet
+    pet.adopter = {
+      _id: user._id,
+      name: user.name,
+      image: user.image
+    }
+
+    await Pet.findByIdAndUpdate(id, pet)
+
+    return res.status(422).json({
+      message: `A visita vou agendada com sucesso, entre em contato com ${pet.user.name} pelo telefone ${pet.user.phone}`
+    });
+
+  }
+
+  
+
   
 }
