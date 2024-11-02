ac83c42ceb6f1aca5914939d6c3c573cf5c96de5
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-02 13:23:21 -0300

    Criação das rotas das páginas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Quero um pet</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
