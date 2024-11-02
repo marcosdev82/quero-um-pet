3f2e2a36a926ae19f61d45653f7dec28660e89a0
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-02 13:57:40 -0300

    Estrutura de navbar e foolter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,10 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+/** COMPONENTS */
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+/** PAGES */
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 import Home from './components/pages/Home';
@@ -6,12 +12,14 @@ import Home from './components/pages/Home';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+       <footer>
+            <p>
+                <span className="bold">Quero um Pet</span> &copy; 2024
+            </p>
+       </footer>
+    )
+}
+
+export default Footer;
