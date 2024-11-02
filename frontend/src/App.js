d8750fe6ca59a8b4891ee1347c251c96b7769ac6
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-02 18:27:01 -0300

    Criação da main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,26 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 /** COMPONENTS */
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Container from './components/layout/Container';
 
 /** PAGES */
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 import Home from './components/pages/Home';
 
+
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes> 
+      </Container>
       <Footer />
     </Router>
   );
