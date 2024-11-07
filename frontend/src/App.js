1fc63117b351768bacbf839bd9f1cd5efead826b
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-07 13:08:01 -0300

    Camando provider no APP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,20 +10,25 @@ import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 import Home from './components/pages/Home';
 
+/** CONTEXT */
+import { UserProvider } from './context/UserContext';
+
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes> 
-      </Container>
-      <Footer />
+      <UserProvider>
+        <Navbar />
+        <Container>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes> 
+        </Container>
+        <Footer />
+      </UserProvider>
     </Router>
   );
 }
