16693bf89fb4f1702b201ca66789c7b713672ad4
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-12 17:31:21 -0300

    Criando os estado da mensagem

--- frontend/src/components/layout/Message.js
+++ frontend/src/components/layout/Message.js
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from './Message.module.css';
+import bus from '../../utils/bus';
 
 function Message() {
+    const [visibility, setVisibility] = useState(false)
+    const [message, setMessage] = useState('')
     const [type, setType] = useState('');  
+
+    useEffect(() => {
+        bus.addListener('flash', ({message, type}) => {
+            setVisibility(true)
+            setMessage(message)
+            setType(type)
+
+            setTimeout(() => {
+                setVisibility(false)
+            }, 3000)
+        });
+    }, []);
 
     return (
         <div className={`${styles.message} ${styles[type]}`}> 
