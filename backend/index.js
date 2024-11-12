949073cef6dbba3fb80ec9689f82594799d3e33a
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-12 13:38:31 -0300

    Mudança de portas e credentials cors origin *

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,26 @@
 const express = require('express'); // Corrigido de 'empress' para 'express'
 const cors = require('cors');
 
-const app = express();
+const app = express(); 
 
 // Config JSON response (Corrigido 'Confi' para 'Config')
-app.use(express.json());
+app.use(express.json()); 
 
 // Solve CORS
-app.use(cors({ credentials: true, origin: 'http://127.0.0.1:4000' }));
+app.use(cors({ credentials: true, origin: '*' }));
+// app.use(cors({ credentials: true, origin: 'http://127.0.0.1:3000' })); 
 
 // Public folder for images
-app.use(express.static('public'));
+app.use(express.static('public')); 
 
 // Routes (Adicione suas rotas aqui)
-const UserRoutes = require('./routes/UserRoutes')
-const PetRoutes = require('./routes/PetRoutes')
+const UserRoutes = require('./routes/UserRoutes');
+const PetRoutes = require('./routes/PetRoutes'); 
 
-app.use('/users', UserRoutes)
-app.use('/pets', PetRoutes)
+app.use('/users', UserRoutes);
+app.use('/pets', PetRoutes); 
 
 // Start server (Adicionei uma mensagem de sucesso)
-app.listen(4000, () => {
-  console.log('Server running on http://127.0.0.1:4000');
+app.listen(4000, () => { 
+  console.log('Server running on http://127.0.0.1:4000'); // Corrigido para a porta correta
 });
