6d9ad6991f09d0f4beac619e1aa9c0bdf0712858
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-13 14:07:38 -0300

    Autenticar ao cadastrar usuário

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,35 +1,35 @@
-import api from '../utils/api'
+import api from '../utils/api';
 
-import { useState, useEffect } from 'react'
-import {useHistory} from 'react-router-dom'
-import useFlashMessage from './useFlashMessage'
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Alteração para useNavigate
+import useFlashMessage from './useFlashMessage';
 
 export default function useAuth() {
-
+    const [authenticated, setAuthenticated] = useState(false);
     const { setFlashMessage } = useFlashMessage();
+    const navigate = useNavigate();
 
-    let msgText = 'Cadastro realizado com sucesso'
-    let msgType = 'success'
+    let msgText = 'Cadastro realizado com sucesso';
+    let msgType = 'success';
 
     async function register(user) {
-
         try {
-            const data = await api.post('/users/register', user).then((response) => {
-                return response.data
-            })
+            const data = await api.post('/users/register', user).then(response => response.data);
 
-            console.log(data)
-        } catch (error)  {
-
-            msgText = error.rempose.data.message
-            msgType = 'error'
-            
+            await authUser(data);
+        } catch (error) {
+            msgText = error.response.data.message;
+            msgType = 'error';
         }
 
-        setFlashMessage(msgText, msgType)
-
+        setFlashMessage(msgText, msgType);
     }
 
-    return { register }
+    async function authUser(data) {
+        setAuthenticated(true);
+        localStorage.setItem('token', JSON.stringify(data.token));
+        navigate('/'); // Alteração para redirecionar com useNavigate
+    }
 
+    return { register };
 }
