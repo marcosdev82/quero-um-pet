7a93301b9708b921ed98bcabb62110911dd63b02
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-13 20:41:51 -0300

    Criação do formulário de login

--- frontend/src/components/pages/Auth/Login.js
+++ frontend/src/components/pages/Auth/Login.js
@@ -6,10 +6,37 @@ import styles from '../../form/Form.module.css'
 /** CONTEXT */
 import { Context } from '../../../context/UserProvider'
 
+import { Link } from 'react-router-dom'
+
 function Login() {
+
+    function handleChange(e) {
+
+    }
+
     return (
-        <section>
+        <section className={styles.form_container}>
             <h1>Login</h1>
+            <form>
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite o seu E-mail"
+                    handleOnChange={handleChange} 
+                />
+                <Input
+                    text="Senha"
+                    type="Password"
+                    name="Password"
+                    placeholder="Digite a sua senha"
+                    handleOnChange={handleChange} 
+                />
+                <input type="submit" value="Entrar" />
+                <p>
+                    Não tem conta? <Link to="/register">Clique aqui.</Link>
+                </p>
+            </form>
         </section>
     )
 }
