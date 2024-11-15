0d43415190c61fa070a89688ae46a25a0e93d5bc
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-14 22:16:41 -0300

    Criação da rota de perfil

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Message from './components/layout/Message';
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 import Home from './components/pages/Home';
+import Profile from './components/pages/User/Profile';
 
 /** CONTEXT */
 import { UserProvider } from './context/UserProvider';
@@ -24,6 +25,7 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/user/profile" element={<Profile />} />
             <Route path="/" element={<Home />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes> 
