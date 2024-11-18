91e9c4e8cd3b37f6a725eb063f1eee2d33cad159
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-18 17:17:05 -0300

    Importação das dependencias

--- frontend/src/components/pages/Pet/AddPet.js
+++ frontend/src/components/pages/Pet/AddPet.js
@@ -1,7 +1,22 @@
+import api from '../../../utils/api'
+
+import styles from './Addpet.module.css'
+
+import { useState } from 'react'
+
+import { redirect } from 'react-router-dom'
+
+/** Hooks */
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
 function AddPet() {
     return (
-        <section>
-            <h1>AddPet</h1>
+        <section className={styles.add_petheader}>
+            <div>
+                <h1>Cadastre um pet</h1>
+                <p>Depois ele ficará disponível para adoção</p>
+            </div>
+            <p>Formulário</p>
         </section>
     )
 }
