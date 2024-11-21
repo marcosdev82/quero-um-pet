54d0a97fff593053b3f98c5aad2b899a52bf429c
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-21 14:06:41 -0300

    Campos do formulário

--- frontend/src/components/form/PetForm.js
+++ frontend/src/components/form/PetForm.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import forStyles from './form.module.css';
+import formStyles from './form.module.css';
 import Input from './input';
 
 function PetForm(handleSubmit, petData, btnTxt) {
@@ -7,11 +7,58 @@ function PetForm(handleSubmit, petData, btnTxt) {
     const [preview, setPreview] = useState([]);
     const colors = ['Preto', 'Branco', 'Cinza', 'Caramelo', 'Mesclado'];
 
-    return (
-        <section>
-                <h1>Pet form</h1>
-        </section>
-    );
+    function onFileChange(e) {
+
+    }
+
+    function handleChange(e) {
+
+    }
+
+    return <form className={formStyles.form_container} >
+        <Input 
+            text="Imagens do pet"
+            type="file"
+            name="images"
+            handleOnChange={onFileChange}
+            multiple={true}
+        />
+        <Input 
+            text="Nome do pet"
+            type="text"
+            name="name"
+            handleOnChange={handleChange}
+            placeholder="Digite o nome"
+            value={pet.name || ''}
+        />
+        <Input 
+            text="Nome do pet"
+            type="text"
+            name="name"
+            handleOnChange={handleChange}
+            placeholder="Digite o nome"
+            value={pet.name || ''}
+        />
+        <Input 
+            text="Idade"
+            type="number"
+            name="age"
+            handleOnChange={handleChange}
+            placeholder="Digite a idade"
+            value={pet.age || ''}
+        />
+         <Input 
+            text="Peso do pet"
+            type="number"
+            name="weight"
+            handleOnChange={handleChange}
+            placeholder="Digite o peso"
+            value={pet.weight || ''}
+        />
+        
+        <input type='submit' value='Cadastrar pet'/>
+
+    </form>
 
 }
 
