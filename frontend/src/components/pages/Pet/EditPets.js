b28ba8ffdbb2830313cb375c011ae8c25e2a82f9
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-28 13:10:25 -0300

    Edit Pet

---
+++ frontend/src/components/pages/Pet/EditPets.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+
+import styles from './Addpet.module.css'
+
+import PetForm from '../../PetForm'
+
+/* hooks */
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
+function EditPet() {
+    return (
+        <section>
+            <h1>EditPet</h1>
+        </section>
+    )
+}
+
+export default EditPet;
