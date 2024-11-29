13d67aefd270466484e04abca8dd9a94a32b2c2e
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-29 13:15:38 -0300

    Update petimplementação do formData

--- frontend/src/components/pages/Pet/EditPet.js
+++ frontend/src/components/pages/Pet/EditPet.js
@@ -29,7 +29,19 @@ function EditPet() {
     }, [token, id])
 
     async function updatePet(pet) {
+        let msgType = 'success'
 
+        const formData = new FormData();
+
+        await Object.keys(pet).forEach((key) => {
+            if (key === 'images') {
+                for (let i = 0; i < pet[key].length; i++) {
+                    formData.append(pet[key][i])
+                }
+            } else {
+                formData.append(key, pet[key])
+            }
+        })
     }
     
     return (
