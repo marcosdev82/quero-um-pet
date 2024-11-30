a1d901e59ac0224d892a419abc7c20d452923910
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-30 15:19:48 -0300

    estilo da home

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -3,8 +3,7 @@ import { useEffect, useState } from 'react'
 
 import { Link } from 'react-router-dom'
 
-import { styles } from './Home.module.css'
-
+import  styles  from './Home.module.css'
 
 function Home() {
     const [pets, setPet] = useState([])
@@ -21,29 +20,36 @@ function Home() {
 
     return (
         <section>
-            <h1>Adote um pet</h1>
-            <p>Veja os detalhes de cada um e conheça o tutor deles</p>
-            <div>
-                {pets.length > 0 && (
+            <div className={styles.pet_home_header}>
+                <h1>Adote um pet</h1>
+                <p>Veja os detalhes de cada um e conheça o tutor deles</p>
+            </div>
+            <div className={styles.pet_container}>
+                {pets.length > 0 ? (
                     pets.map((pet) => (
-                        <div>
-                            <p>Image do pet</p>
-                            <h3>{pet.name}</h3>
-                            <p>
-                                <span className='bold'>Pesso:</span> {pet.weight}kg
-                                {pet.avilable ? (
-                                    <Link to={`pet/${pet._id}`} >Mais detalhes</Link>
-                                ) : (
-                                    <p>Adotado</p>
-                                ) } 
-                            </p>
-                        </div>
+                    <div className={styles.pet_card} key={pet._id}>
+                        <div
+                        className={styles.pet_card_image}
+                        style={{
+                            backgroundImage: `url(${process.env.REACT_APP_API}/images/pets/${pet.images[0]})`,
+                        }}
+                        ></div>
+                        <h3>{pet.name}</h3>
+                        <p>
+                        <span className="bold">Peso:</span> {pet.weight}kg
+                        </p>
+                        {pet.available ? (
+                        <Link to={`pet/${pet._id}`}>Mais detalhes</Link>
+                        ) : (
+                        <p className={styles.adopted_text}>Adotado</p>
+                        )}
+                    </div>
                     ))
-                )}
-                {pets.length === 0 && (
+                ) : (
                     <p>Não há pets cadastrados ou disponíveis nesse momento!</p>
                 )}
-            </div>
+                </div>
+
         </section>
     )
 }
